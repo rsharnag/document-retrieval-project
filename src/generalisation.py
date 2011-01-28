5d25776647398ede501cb39d4550491bccf69cf5
Rahul Sharnagat <rdsharnagat@gmail.com>
2011-01-28 23:55:35 +0530

    Added kd-tree module.

---
+++ src/generalisation.py
@@ -0,0 +1,37 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="aditya & rahul"
+__date__ ="$26 Jan, 2011 4:13:20 PM$"
+
+from nltk.corpus import wordnet as wn
+def createSynsetDict(wordList):
+    """Creates a dictionary of synonyms of given wordlist"""
+    synWords={}
+    for word in wordList:
+        synWords[word] = wn.synsets(word)
+    return synWords
+
+def generalisation(originalWordList):
+    generalisedWordList=[]
+    currentWordList = originalWordList
+    synWords = createSynsetDict(originalWordList)
+    while(currentWordList.size()>2):
+        generalizedWords = []
+        currentWord = currentWordList.pop()
+        for otherWord in currentWordList:
+            for currentWordSyn in synWords[currentWord]:
+                for ithWordSyn in synWords[otherWord]:
+                    short_dist = ithWordSyn.shortest_path_distance(currentWordSyn)
+                    if (short_dist != None and short_dist <= 2):
+                        generalizedWords.append(ithWordSyn.common_hypernyms(currentWordSyn))
+           
+            generalisedWordList.extend([currentWord,otherWord])
+            if(len(generalizedWords)!=0):
+                currentWordList.remove(otherWord)
+                l=[]
+                for syns in generalisedWords:
+                    l.append(syns.lemma_names)
+                generalisedWordList.append(l)
+    generalisedWordList=list(set(generalisedWordList))
+    return generalisedWordList
---
+++ src/setup.py
@@ -0,0 +1,27 @@
+__author__="aditya"
+__date__ ="$27 Jan, 2011 11:37:48 PM$"
+
+from setuptools import setup,find_packages
+
+setup (
+  name = 'Tagger',
+  version = '0.1',
+  packages = find_packages(),
+
+  # Declare your packages' dependencies here, for eg:
+  install_requires=['foo>=3'],
+
+  # Fill in these to make your Egg ready for upload to
+  # PyPI
+  author = 'aditya',
+  author_email = '',
+
+  summary = 'Just another Python package for the cheese shop',
+  url = '',
+  license = '',
+  long_description= 'Long description of the package',
+
+  # could also include long_description, download_url, classifiers, etc.
+
+  
+)
