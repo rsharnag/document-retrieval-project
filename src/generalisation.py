29f7e0a3fb835d4f1f903be4e3c6e4af2414546f
aditya <aditya@aditya-laptop>
2011-01-29 11:39:59 +0530

    1) Introducing a doc-processor class 2) Generalization Algorithm v2 3) Out of tagged words to a file "output"

--- src/generalisation.py
+++ src/generalisation.py
@@ -4,7 +4,7 @@
 __author__="aditya & rahul"
 __date__ ="$26 Jan, 2011 4:13:20 PM$"
 
-from nltk.corpus import wordent as wn
+from nltk.corpus import wordnet as wn
 def createSynsetDict(wordList):
     """Creates a dictionary of synonyms of given wordlist"""
     synWords={}
@@ -16,22 +16,27 @@ def generalisation(originalWordList):
     generalisedWordList=[]
     currentWordList = originalWordList
     synWords = createSynsetDict(originalWordList)
-    while(currentWordList.size()>2):
+    while(len(currentWordList)>2):
         generalizedWords = []
+        currShortest_Dist = 2
         currentWord = currentWordList.pop()
         for otherWord in currentWordList:
             for currentWordSyn in synWords[currentWord]:
                 for ithWordSyn in synWords[otherWord]:
                     short_dist = ithWordSyn.shortest_path_distance(currentWordSyn)
-                    if (short_dist != None and short_dist <= 2):
-                        generalizedWords.append(ithWordSyn.common_hypernyms(currentWordSyn))
-           
+                    if (short_dist != None and short_dist < currShortest_Dist):
+                        currShortest_Dist = short_dist
+                        generalizedWords = []
+                        generalizedWords.extend(ithWordSyn.common_hypernyms(currentWordSyn))
+                    if (short_dist != None and short_dist == currShortest_Dist):
+                        generalizedWords.extend(ithWordSyn.common_hypernyms(currentWordSyn))
+                        
             generalisedWordList.extend([currentWord,otherWord])
             if(len(generalizedWords)!=0):
                 currentWordList.remove(otherWord)
                 l=[]
-                for syns in generalisedWords:
-                    l.append(syns.lemma_names)
-                generalisedWordList.append(l)
+                for syns in generalizedWords:
+                    l.extend(syns.lemma_names)
+                generalisedWordList.extend(l)
     generalisedWordList=list(set(generalisedWordList))
     return generalisedWordList
