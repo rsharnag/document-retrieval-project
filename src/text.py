f83dad8eab09a77ecf2d27a750c9b38bc996ed09
Rahul Sharnagat <rdsharnagat@gmail.com>
2011-02-17 20:29:28 +0530

    1]Debugged hasher.py and tagger.py (according to me fully functional but please check and report errors or if possible correct them  ) 2]Added two new dump maps (in hope that it will save memory) 	1)file_map_dump = maps filename to integer 	2)file_id_dump = maps integer to filename (reverse of above) 3]Added fileopen error handling wherever possible.

--- src/text.py
+++ src/text.py
@@ -5,6 +5,10 @@ __author__="aditya"
 __date__ ="$23 Jan, 2011 4:56:11 PM$"
 class ReadText:
     def __init__(self,filename="taggerText"):
-        inputFile=open(filename,"r")
+        try:
+            inputFile=open(filename,"r")
+        except IOError:
+            print "File not found"
+            exit(1)
         self.readAll=inputFile.read()
         inputFile.close()
