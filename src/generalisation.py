076365cde2678654d46326814e67a227d349fd1b
Aditya Tayade <aktayade@gmail.com>
2011-03-06 14:00:52 +0530

    intermediate

--- src/generalisation.py
+++ src/generalisation.py
@@ -16,10 +16,11 @@ def generalisation(originalWordList):
     generalisedWordList=[]
     currentWordList = originalWordList
     synWords = createSynsetDict(originalWordList)
-    while(len(currentWordList)>2):
+    while(len(currentWordList)>=2):
         generalizedWords = [[],[],[]]
         #currShortest_Dist = 2
         currentWord = currentWordList.pop()
+        generalisedWordList.extend([currentWord])
         for otherWord in currentWordList:
             for currentWordSyn in synWords[currentWord]:
                 for ithWordSyn in synWords[otherWord]:
@@ -27,20 +28,27 @@ def generalisation(originalWordList):
                     #print short_dist
                     if (short_dist != None and short_dist <= 2):
                         generalizedWords[short_dist] = generalizedWords[short_dist] + ithWordSyn.common_hypernyms(currentWordSyn)
-        generalisedWordList.extend([currentWord])
         l=[]
         if(len(generalizedWords[0])!= 0):
             currentWordList.remove(otherWord)
+            generalisedWordList.extend([otherWord])
             for syns in generalizedWords[0]:
                 l.extend(syns.lemma_names)
         elif (len(generalizedWords[1])!= 0):
             currentWordList.remove(otherWord)
+            generalisedWordList.extend([otherWord])
             for syns in generalizedWords[1]:
                 l.extend(syns.lemma_names)
         elif (len(generalizedWords[2])!= 0):
             currentWordList.remove(otherWord)
+            generalisedWordList.extend([otherWord])
             for syns in generalizedWords[2]:
                 l.extend(syns.lemma_names)
         generalisedWordList.extend(l)
+    generalisedWordList.extend(currentWordList)
     generalisedWordList=list(set(generalisedWordList))
+    open("output_generalizedWL","w").write(str(generalisedWordList))
     return generalisedWordList
+
+if __name__ == "__main__":
+    generalisation(['cell', 'anatomy', 'biology', 'knowledge', 'chromosome'])
